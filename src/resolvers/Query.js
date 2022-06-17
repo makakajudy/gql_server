b8016646b504e524be21edea99078bd43d5134a1
tahmidh <tahmidh@softpauer.com>
2022-06-17 17:06:27 +0100

    update

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,34 +2,35 @@
 
 function info() { return `This is the API of a Hackernews Clone`} 
 
- async function feed(parent, args, context) { 
-   //def {has to be a where clause} an obj that returns empty obj if no match is founs or an obj
-   //with array of data that meet the condition
-   //condition expression if arg.filter is true
-  //truth-if the dec OR url conatins the filer arg asign it to the filterObj
-  const where =args.filter
-   ?{
-    OR:
-    [{description:{contains:args.filter}},
-      {url:{contains:args.filter}},
-       ],
-       }
-       :{}
-       //pass the filtrObj to the finadmany()
-  const filter_results = await context.prisma.link.findMany({
-    where,
-    skip:args.skip,
-    take:args.take,
-    orderBy:args.orderBy,
+async function feed(parent, args, context) { 
+  //def {has to be a where clause} an obj that returns empty obj if no match is founs or an obj
+  //with array of data that meet the condition
+  //condition expression if arg.filter is true
+ //truth-if the dec OR url conatins the filer arg asign it to the filterObj
+const where =args.filter
+  ?{
+   OR:
+   [{description:{contains:args.filter}},
+     {url:{contains:args.filter}},
+      ],
+      }
+      :{}
+      //pass the filtrObj to the finadmany()
+      const links =await context.prisma.link.findMany({
+   where,
+   skip:args.skip,
+   take:args.take,
+   orderBy:args.orderBy,
 
-  })
+ })
+
   const count=await context.prisma.link.count({where})
-
-  return {
-    filter_results,//not working as it should returns null list
+return {
+    links,//not working as it should returns null list
     count,
      }
 }
+
 
 function all_students (parent, args, context)  {
     return context.prisma.student_info.findMany()
